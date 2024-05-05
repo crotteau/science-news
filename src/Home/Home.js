937621ef9a4b3583afff052a90d1dbf9db8b1c98
Megan Crotteau <megan.crotteau@gmail.com>
2024-05-05 13:03:04 -0600

    Header styling adjustment

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,19 +2,15 @@ import './Home.css';
 import React from 'react';
 import Articles from '../Articles/Articles';
 import Country from '../Country/Country';
-import cluster2 from '../assets/design2.png';
-import cluster1 from '../assets/design1.png';
 import rainbow from '../assets/rainbow.png';
 
 function Home({ articles, setCountry }) {
 
     return (
         <section className='home'>
-            <header>
+            <header style={{ backgroundImage: `url(${rainbow})` }}>
                 <h2 className='home-header'>SCIENCE NEWS</h2>
                 <img src={rainbow} className='rainbow' />
-                <img src={cluster2} className='cluster2' />
-                <img src={cluster1} className='cluster1' />
             </header>
             <Country setCountry={setCountry} />
             <Articles articles={articles} />
